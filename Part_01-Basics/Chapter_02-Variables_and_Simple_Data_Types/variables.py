4d482272a2caa71f33c00bd6dd83452447db6f98
Dimitri Nakos <dimi1nakos@gmail.com>
2021-11-07 17:18:33 -0500

    started Chapter 2 and finishing when I get some quiet time.

---
+++ Part_01-Basics/Chapter_02-Variables_and_Simple_Data_Types/variables.py
@@ -0,0 +1,10 @@
+# Variables
+message = "Hello Github World :)"
+print(message)
+
+message = "Hello Python Crash Course"
+print(message)
+
+message = "Reassigning variables gives them new value."
+print(message)
+
