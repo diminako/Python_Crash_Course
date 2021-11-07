5ab6bba9d7736387eef4ef0f9d4601d042a206d8
Dimitri Nakos <diminako@Dimitris-MacBook-Air.local>
2021-11-07 16:48:33 -0500

    Chapter 01 - Installation.

---
+++ Part_01-Basics/Chapter_01-Getting_Started/getting_started.py
@@ -0,0 +1,12 @@
+print("Hello World.  lol.  Nothing like the Basics.")
+
+# I'm not skipping anything in this book so I'm staarting from scratch.  Hence Hellow World :)
+
+# Explore Python.org
+
+# Generate Hello World typos to see what errors and how to diagnose them using the error codes.
+
+# Think of 3 different Projects to make at the end of this book.
+    # Web App to keep track of savings.
+    # Tactical RPG
+    # Script to analyze information and write it to another file.
