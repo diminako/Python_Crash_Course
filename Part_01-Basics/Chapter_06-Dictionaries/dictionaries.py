a539b19795a410da4ccc422ea1b22f2e27706023
Dimitri Nakos <dimi1nakos@gmail.com>
2021-11-16 23:08:55 -0500

    dictionary exercises.

--- Part_01-Basics/Chapter_06-Dictionaries/dictionaries.py
+++ Part_01-Basics/Chapter_06-Dictionaries/dictionaries.py
@@ -78,5 +78,78 @@ print(favorite_languages.get('edward', "No value assigned"))
 print(favorite_languages.get('edward')) #  Not assigning a value to the backup value with return None "No value exists"
 
 
+# looping through a dictionary
 
+# looping through all key-value pairs
+user_0 = {
+    'name': 'diminako',
+    'first': 'dimitri',
+    'last': 'nakos',
+}
+print(user_0.items()) # prints out the items(key and values) inside a dictionary.
 
+for key, value in user_0.items():
+    print(key, value)
+
+for person, favorite in favorite_languages.items():
+    print(f"{person.title()} loves using {favorite.title()} to code in.")
+
+# looping through all keys in a dictionary
+for name in favorite_languages.keys(): # loop through keys
+    print(name.title())
+for name in favorite_languages.values(): # loop through values
+    print(name.title())
+
+# looping and finding associated elements in a dictionary and a list
+favorite_languages = {
+    'dimitri': 'javascript',
+    'emily': 'ruby',
+    'john': 'c',
+    'brian': 'sql',
+}
+friends = ['dimitri', 'john']
+
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you like {language}!  Cool!")
+
+if 'erin' not in favorite_languages.keys():
+    print('Erin please take our poll.')
+
+# Looping through a Dictionary's keys in a particular order.
+cars = {
+    'vw': 'tiguan',
+    'dodge': 'ram',
+    'toyota': 'tundra',
+    'bmw': 'm3',
+}
+
+for manufacturer in sorted(cars.keys()): # Sorts the dictionary before performing the loop. doesn't save it that way.
+    print(f"{manufacturer.title()}")
+print(cars.keys())
+
+# looping through values in a dictionary
+print("These are the selection of cars for today.")
+for car in cars.values():
+    print(car.title())
+print('I want those in alphabetical order tho!')
+for car in sorted(cars.values()):
+    print(car.title())
+
+fav_languages = {
+    'dimitri': 'python',
+    'brian': 'java',
+    'john': 'python',
+    'emiy': 'java',
+    'ronnie': 'ruby',
+    'ronnie': 'ruby',
+}
+print("These languages were mentioned.  Some more than once.")
+for lang in set(fav_languages.values()):
+    print(lang)
+
+# Sets are similar to dicts but they have no value.  and will only display multiples of the same elemets once.
+this_set_not_dict = {'python', 'ruby', 'python', 'python'}
+print(this_set_not_dict)
--- Part_01-Basics/Chapter_06-Dictionaries/exercises_06/dicts.py
+++ Part_01-Basics/Chapter_06-Dictionaries/exercises_06/dicts.py
@@ -35,6 +35,61 @@ glossary = {
     'if/else': 'logic performed depending on a True of False statement.',
     'dictionary': 'a way to organize a key-value pair.',
 }
-
 for key in glossary:
     print(f"a(n) {key} is {glossary.get(key)}")
+
+# Exercise 6-4 Glossary 2 - add 5 more key-value pairs to glossary find a way to loop through in a clean way.
+glossary = {
+    'variables': 'keyword for a stored value.',
+    'lists': 'an array of elements stored in an order.',
+    'boolean': 'a value of True or False.',
+    'if/else': 'logic performed depending on a True of False statement.',
+    'dictionary': 'a way to organize a key-value pair.',
+    'set': 'a way to provide unique elements from duplicates.',
+    'operators': 'are different ways to manipulate elements.',
+    'values': 'are stored elements.',
+    'keys': 'a type of variable holding a value.',
+    'loops': 'a way process lists and dicts.',
+}
+
+print(f"These are key words in python and their meaning. Sorted alphabetically.")
+for words, meaning in sorted(glossary.items()):
+    print(f"{words}: {meaning}")
+# Exercise 6-5 Rivers - make a dict containing three major rivers and the country they run through. example 'nile': 'egypt'
+# loop making a sentence about each.  loop the name of each river. loop to print the country.
+rivers = {
+    'nile': 'egypt',
+    'amazon': 'brazil',
+    'euphrates': 'greece',
+    'mississippi': 'usa',
+}
+for river, country in rivers.items():
+    print(f"The {river.title()} flows through {country.title()}.")
+
+for river in rivers.keys():
+    print(river)
+
+for country in rivers.values():
+    print(country)
+
+# Exercise 6-6 Polling - use fav_langs and create a dict. make a list of people iin and not in fav_langs.  loop thru. If they
+# have a fav lang print their fav lang if not print that they should take the poll.
+fav_langs = {
+    'dimitri': 'python',
+    'john': 'javascript',
+    'emily': 'english',
+    'brian': 'mysql',
+}
+poll_people = ['ronnie', 'dimitri', 'john', 'josh', 'andrew']
+
+for people in poll_people:
+    if people in fav_langs.keys():
+        print(f"Nice {people}, You like {fav_langs.get(people)}.")
+    else:
+        print(f"{people}, why haven't you taken the poll yet?")
+
+# for people, langs in fav_langs.items():           # This method looped through the fav_langs dict and missed people in the poll list
+#     if people in poll_people:
+#         print(f"Nice {people}, You like {langs}.")
+#     else:
+#         print(f"{people}, why haven't you taken the poll yet?")
