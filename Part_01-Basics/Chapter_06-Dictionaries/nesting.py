2013c2f4836abe1ed67c648a12f177cbdfcdf90f
Dimitri Nakos <dimi1nakos@gmail.com>
2021-11-17 09:18:11 -0500

    first mental hickups :)

--- Part_01-Basics/Chapter_06-Dictionaries/nesting.py
+++ Part_01-Basics/Chapter_06-Dictionaries/nesting.py
@@ -11,8 +11,8 @@ for alien in aliens:
 # Make an empty list for storing aliens
 aliens = []
 
-# Make 30 green aliens
-for alien_number in range(30):
+# Make 10 green aliens
+for alien_number in range(10):
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 
@@ -23,3 +23,24 @@ print("...")
 
 # show how many aliens there are.
 print(f'Total number of aliens: {len(aliens)}')
+
+# alter the fir 3 aliens
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+for alien in aliens[:5]:
+    print(alien)
+print('...')
+
+# A list in a dictionary
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+# summarize the order
+print(f"You ordered a {pizza['crust']}-crust pizza. With the following toppings:")
+for topping in pizza['toppings']:
+    print(f'\t{topping}')
+
