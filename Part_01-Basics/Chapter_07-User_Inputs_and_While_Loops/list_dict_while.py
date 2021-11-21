b4ed46ad5cd851187a6fc9174c0024c25ff542e3
Dimitri Nakos <dimi1nakos@gmail.com>
2021-11-21 08:30:29 -0500

    coffe break.

--- Part_01-Basics/Chapter_07-User_Inputs_and_While_Loops/list_dict_while.py
+++ Part_01-Basics/Chapter_07-User_Inputs_and_While_Loops/list_dict_while.py
@@ -14,3 +14,33 @@ while unconfirmed_users:
 print("\nThe following users have been confirmed:")
 for confirmed_user in confirmed_users:
     print(confirmed_user)
+
+# Removing all instances of specified values from a list
+pets = ['dog','cat', 'goldfish', 'rabbit', 'cat', 'cat', 'lizard']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
+
+# Filling a dictionary with user input.
+responses = {}
+# Set a flag to indicate tht polling is active.
+polling_active = True
+
+while polling_active:
+    # Prompt for the users name and response.
+    name = input("\nWhat is your name?  ")
+    response = input("Whic mountain would you like to climb someday?  ")
+    # store the responses in the dictionary
+    responses[name] = response
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond?  (yes/no)  ")
+    if repeat == 'no':
+        polling_active = False
+
+# Polling is complete.  Show the results.
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(f"{name.title()} would like to climb {response}.")
