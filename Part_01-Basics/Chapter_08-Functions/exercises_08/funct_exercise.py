1020c0106c41ed43220a8812f9d96e9730e6fd74
Dimitri Nakos <dimi1nakos@gmail.com>
2021-11-22 16:44:02 -0500

    too hungry to continue.

--- Part_01-Basics/Chapter_08-Functions/exercises_08/funct_exercise.py
+++ Part_01-Basics/Chapter_08-Functions/exercises_08/funct_exercise.py
@@ -9,3 +9,23 @@ def favorite_book(title):
     print(f"my favorite book is {title.title()}.")
 
 favorite_book('The Shining')
+
+# Exercise 8-3 T-Shirt - write a funct make_shirt() pass size and text.  print a sentence saying the size and message on it.  use positional and keyword arguments.
+def make_shirt(size, label):
+    print(f"This shirt is a {size} with the slogan '{label}.'")
+
+make_shirt('small', 'sick shirt')
+make_shirt(size='medium', label="That's pretty neat.")
+
+# Exercise 8-4 Large Shirts - modify the last exercise for a large shirt that says i love python.  a medium shirt that has a default message and a shirt of any size that has a diff message.
+make_shirt(size="Large", label="I Love Python")
+make_shirt("Large", label="sick shirt")
+make_shirt(size="medium", label="That's Pretty Neat")
+
+# Exercise 8-5 Cities - write a funct called describe_city() accepts a city and country.  print a sentence. give the country a default value.
+def describe_city(city, country):
+    print(f"{city.title()} is in {country.title()}")
+
+describe_city('Athens', 'Greece')
+describe_city('Thessaloniki', 'Greece')
+describe_city('Charlotte', country='Greece')
--- Part_01-Basics/Chapter_08-Functions/functions.py
+++ Part_01-Basics/Chapter_08-Functions/functions.py
@@ -12,3 +12,21 @@ def greet_user(username):
 
 greet_user("Dimitri")
 
+# Passing arguments
+def describe_pet(animal_type, pet_name):
+    '''Display information about a pet.'''
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('hamster', 'harry')  # Order matters in positional arguments.
+describe_pet('dog', 'tunde')  # Order matters in positional arguments.
+
+# Keyword arguments - name value pair that you pass into a function
+def describe_pet(animal_type, pet_name):
+    '''Display information about a pet.'''
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet(animal_type='giraffe', pet_name='harry')  # Order matters in positional arguments.
+describe_pet(animal_type='donkey', pet_name='shrek')  # Order matters in positional arguments.
+
