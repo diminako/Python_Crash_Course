83206468219a97cc4356557fc11a7769306e7249
Dimitri Nakos <dimi1nakos@gmail.com>
2021-11-30 18:23:17 -0500

    working with data in files.

--- Part_01-Basics/Chapter_10-Files_and_Exceptions/files_and_exceptions.py
+++ Part_01-Basics/Chapter_10-Files_and_Exceptions/files_and_exceptions.py
@@ -24,3 +24,44 @@ with open(file_path) as file_object:
         print(line.rstrip())  # Removes the extra line caused by the text files.
 
 # Modifying a list of lines from a file.
+file_name = 'text_files/filename.txt'
+with open(file_name) as file:
+    lines = file.readlines()
+
+for line in lines:
+    print(line.rstrip())
+
+print(lines)
+
+# working with a file's contents 
+pi_file = 'pi_digits.txt'
+
+with open(pi_file) as pi_txt:
+    lines = pi_txt.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()  # or using .strip() to remove all whitespace.
+
+print(pi_string)
+print(len(pi_string))
+
+# Large files: One million digits
+filename = 'text_files/one_million_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(f'{pi_string[:52]}...')
+print(len(pi_string))
+
+# Is my birthday in pi?
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("You are not special.")
