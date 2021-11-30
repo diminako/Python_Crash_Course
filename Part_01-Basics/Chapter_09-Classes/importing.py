60cf55a0365f6e077e4d19c0b7027b42907360dd
Dimitri Nakos <dimi1nakos@gmail.com>
2021-11-29 19:33:03 -0500

    importing modules.

--- Part_01-Basics/Chapter_09-Classes/importing.py
+++ Part_01-Basics/Chapter_09-Classes/importing.py
@@ -9,4 +9,16 @@ print(dimi_car.get_descriptive_name())
 dimi_car.odometer_reading = 80
 dimi_car.read_odometer()
 
-# Storing multiple classes in a module.
+# Storing multiple classes in a module.  and importing multiple classes from a module.
+from base_car import Battery, Electric
+my_tesla = Electric('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+
+# Importing an Entire module
+import base_car
+my_beetle = base_car.Car('Volkswagen', 'beetle', 1974)
+dimis_tesla = base_car.Electric('tesla', 'model s', 2019)
+print(my_beetle.get_descriptive_name())
+print(dimis_tesla.get_descriptive_name())
